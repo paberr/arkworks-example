ba2110c8042192ab8028a759e58603b7f4e456e8
Pascal Berrang <git@paberr.net>
2023-03-09 15:47:25 -0600

    Minimum failing example

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use ark_ff::{PrimeField, Zero};
+use ark_mnt6_753::Fq;
+
+fn main() {
+    let a = Fq::zero();
+    a.into_bigint();
+}
---
+++ tests/test.rs
@@ -0,0 +1,8 @@
+use ark_ff::{PrimeField, Zero};
+use ark_mnt6_753::Fq;
+
+#[test]
+fn foobar() {
+    let a = Fq::zero();
+    a.into_bigint();
+}
